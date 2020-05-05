99c8c737d451629d0e46c29947d53e1a14051418
Anton Shtanyuk <ashtanyuk@gmail.com>
2020-05-05 16:52:55 +0300

    Initial commit

---
+++ test/tests.cpp
@@ -0,0 +1,53 @@
+#include "gtest/gtest.h"
+#include <string>
+#include "tpqueue.h"
+
+
+TEST(lab5,test1_1)
+{
+    TPQueue<SYM> pqueue;
+    pqueue.push(SYM{'a',4});
+    pqueue.push(SYM{'b',7});
+    SYM c1=pqueue.pop();
+    SYM c2=pqueue.pop();
+    ASSERT_EQ(c1.ch,'b');
+    ASSERT_EQ(c2.ch,'a');
+}
+
+TEST(lab5,test1_2)
+{
+    TPQueue<SYM> pqueue;
+    pqueue.push(SYM{'a',4});
+    pqueue.push(SYM{'b',4});
+    SYM c1=pqueue.pop();
+    SYM c2=pqueue.pop();
+    ASSERT_EQ(c1.ch,'a');
+    ASSERT_EQ(c2.ch,'b');
+}
+
+TEST(lab5,test1_3)
+{
+    TPQueue<SYM> pqueue;
+    pqueue.push(SYM{'a',4});
+    pqueue.push(SYM{'b',4});
+    pqueue.push(SYM{'c',9});
+    SYM c1=pqueue.pop();
+    SYM c2=pqueue.pop();
+    SYM c3=pqueue.pop();
+    ASSERT_EQ(c1.ch,'c');
+    ASSERT_EQ(c2.ch,'a');
+    ASSERT_EQ(c3.ch,'b');
+}
+TEST(lab5,test1_4)
+{
+    TPQueue<SYM> pqueue;
+    for(int i=1;i<=10000;i++)
+       pqueue.push(SYM{'a',4});
+
+    pqueue.push(SYM{'b',7});
+   
+    SYM c1=pqueue.pop();
+    SYM c2=pqueue.pop();
+    ASSERT_EQ(c1.ch,'b');
+    ASSERT_EQ(c2.ch,'a');
+}
